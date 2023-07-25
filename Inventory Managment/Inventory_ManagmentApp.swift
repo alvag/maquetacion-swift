feaed82a5e64de0c6ac255940e57de02d6805bab
Max Alva <alva85@gmail.com>
2023-07-25 17:51:17 -0500

    Initial Commit

---
+++ Inventory Managment/Inventory_ManagmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inventory_ManagmentApp.swift
+//  Inventory Managment
+//
+//  Created by Max Alva on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Inventory_ManagmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
